f53915dfcb27f7dacfb99078b686ede3f23452b6
lynnshahuang <jing01ucsb@gmail.com>
2018-06-09 01:17:47 -0700

    Upload Hw# 2

--- Homework#2.py
+++ Homework#2.py
@@ -2,18 +2,18 @@
 # Name: Alison Jing Huang -- jing01ucsb@gmail.com
 # ID: X111012
 # Python for Data Analysis and Scientific Computing HW #2
-
 import numpy as np
+from numpy import matrix, array, random, min, max
 
 # Create a matrix A with size (3,5) containing random numbers
 # A= np.random.random(15)
 
-A = np.random.random(15)
-A.reshape(3,5)
+A = random.random(15)
+A = A.reshape(3,5)
+A = matrix(A)
 
 # Find the size and length of matrix A.
 A.size
-A.shape
 len(A)
 
 # Resize(crop/slice) matrix A to size (3,4)
@@ -23,7 +23,7 @@ A = A[0:3,0:4]
 B = A.transpose()
 
 # Find the minimum value in column 1 of matrix B(check the properties of a matrix -'B.min()')
-min(A[:,0])
+B[:,1].min()
 
 # Find the minimum and maximum values for the entire matrix A
 
@@ -31,29 +31,31 @@ A.min()
 A.max()
 
 # Create Vector X(an array) with 4 random numbers
-X = np.random.random(4)
+X = array([random.random(4)])
 
 
 # Create a function and pass vector X and matrix A in it
 # In the new function multiply vector X and matrix A and assign the result
 # to D.(note: you may get an error! - think why and fix it. Recall manipulation in class!)
 
-def function(X,A):
-    D = X*A
+def function_XA(a,b):
+    return a*b.T
+
+D = function_XA(X,A)
 
 # Create a complex number Z with absolute and real parts !=0
 
-    Z = np.csingle(60+ 129.89185j)
+Z = 200 + 60j
 
 # Show its real and imaginary parts as well as its absolute value
 
-    print(Z.real)
-    print(Z.imag)
-    print(np.absolute(Z))
+print(Z.real)
+print(Z.imag)
+print(np.absolute(Z))
 
 # Multiply result D with the absolute value of Z and record it to C
 
-    C = D*np.absolute(Z)
+C = D * np.abs(Z)
 
 # Convert matrix B from a matrix to a string and overwrite B
 
