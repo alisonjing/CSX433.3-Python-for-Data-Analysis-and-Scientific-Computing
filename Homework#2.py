3b5a90a590d20001dfbf783612782978daca2f7f
lynnshahuang <jing01ucsb@gmail.com>
2018-06-09 01:28:10 -0700

    Update Homework#2

--- Homework#2.py
+++ Homework#2.py
@@ -1,31 +1,33 @@
+#1. Include a section line with your name:
 
 # Name: Alison Jing Huang -- jing01ucsb@gmail.com
 # ID: X111012
 # Python for Data Analysis and Scientific Computing HW #2
+
 import numpy as np
 from numpy import matrix, array, random, min, max
 
-# Create a matrix A with size (3,5) containing random numbers
+#2. Create a matrix A with size (3,5) containing random numbers
 # A= np.random.random(15)
 
 A = random.random(15)
 A = A.reshape(3,5)
 A = matrix(A)
 
-# Find the size and length of matrix A.
+#3.  Find the size and length of matrix A.
 A.size
 len(A)
 
-# Resize(crop/slice) matrix A to size (3,4)
+#4. Resize(crop/slice) matrix A to size (3,4)
 A = A[0:3,0:4]
 
-# Find the transpose of matrix A and assign it to B
+#5.  Find the transpose of matrix A and assign it to B
 B = A.transpose()
 
-# Find the minimum value in column 1 of matrix B(check the properties of a matrix -'B.min()')
+#6.  Find the minimum value in column 1 of matrix B(check the properties of a matrix -'B.min()')
 B[:,1].min()
 
-# Find the minimum and maximum values for the entire matrix A
+#7. Find the minimum and maximum values for the entire matrix A
 
 A.min()
 A.max()
@@ -34,7 +36,7 @@ A.max()
 X = array([random.random(4)])
 
 
-# Create a function and pass vector X and matrix A in it
+#8. Create a function and pass vector X and matrix A in it
 # In the new function multiply vector X and matrix A and assign the result
 # to D.(note: you may get an error! - think why and fix it. Recall manipulation in class!)
 
@@ -43,25 +45,32 @@ def function_XA(a,b):
 
 D = function_XA(X,A)
 
-# Create a complex number Z with absolute and real parts !=0
+#9. Create a complex number Z with absolute and real parts !=0
 
 Z = 200 + 60j
 
-# Show its real and imaginary parts as well as its absolute value
+#10, 11, 12.  Show its real and imaginary parts as well as its absolute value
 
 print(Z.real)
 print(Z.imag)
 print(np.absolute(Z))
 
-# Multiply result D with the absolute value of Z and record it to C
+#13.  Multiply result D with the absolute value of Z and record it to C
 
 C = D * np.abs(Z)
 
-# Convert matrix B from a matrix to a string and overwrite B
+#14.  Convert matrix B from a matrix to a string and overwrite B
 
 B = str(B)
 
-# Display a text on the screen: 'Your Name is done with HW2'
+#15. Display a text on the screen: 'Your Name is done with HW2'
 
 name = "Alison Jing Huang"
 print(name + " is done with HW2")
+
+#16. Organize your code: use each line from this assignment as a comment line before each step
+#17. Save all steps as a script in a .py file
+#18. Email your Github link to me including your .py file + screenshots of your running code no later than midnight on Saturday Jun.09.
+
+
+
